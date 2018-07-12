b9f0fcc7dd23c275d00860903953f34649bb1354
bradcgarner <brad@bradgarner.com>
2018-07-11 22:14:22 -0400

    converted to ES5

---
+++ src/icons.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const icons = () => ({
+  home:      'fas fa-home',
+  test:      'fas fa-cloud-download-alt',
+  created:   'fas fa-flag-checkered',
+  built:     'fas fa-flag-checkered',
+  start:     'fas fa-tint', // water drop
+  on:        'fas fa-arrow-right',
+  off:       'fas fa-times-circle',
+  dismantled:'fas fa-times-circle',
+  end:       'fas fa-arrow-left',
+  cassette:  'fas fa-inbox',
+  component: 'fas fa-puzzle-piece',
+  slope:     'fas fa-level-up-alt', // _|^
+  rain:      'fas fa-shower',       // showerhead
+  shower:    'fas fa-shower',
+  notes:     'fas fa-comment-alt',
+  raindrop:  'fas fa-tint', // water drop
+  absorb:    'fas fa-tint', // water drop
+  stopwatch: 'fas fa-stopwatch',
+  runoff:    'fas fa-tint', // water drop
+  clock:     'fas fa-clock',
+  retention: 'fas fa-glass-martini',
+  delay:     'fas fa-hand-paper', // halt hand signal
+  label:     'fas fa-tags',
+  size:      'fas fa-object-group',
+  layers:    'fas fa-bars',
+  scale:     'fas fa-weight',
+  plants:    'fas fa-leaf',
+  media:     'fas fa-braille',
+  thickness: 'fas fa-text-height',
+  zones:     'fas fa-share-alt',
+  login:     'fas fa-user-plus',
+  logout:    'fas fa-user-times',
+  monitor:   'fas fa-heartbeat',
+  contain:   'fas fa-inbox',
+  check:     'fas fa-check',
+  refresh:   'fas fa-sync',
+  reorder:   'fas fa-retweet',
+  up:        'fas fa-arrow-up',
+  dn:        'fas fa-arrow-down',
+  close:     'fas fa-times',
+  trash:     'fas fa-trash-alt',
+  lock:      'fas fa-lock',
+  unlock:    'fas fa-unlock',
+  save:      'fas fa-save',
+  error:     'fas fa-exclamation-triangle',
+  paper:     'fas fa-image',
+  print:     'fas fa-file',
+  question:  'fas fa-question',
+  search:    'fas fa-search',
+  right:     'fas fa-caret-right',
+  left:      'fas fa-caret-left',
+  pdf:       'fas fa-file-pdf',
+  file:      'fas fa-file-alt',
+  pressure:  'fas fa-bomb',
+  temperature:'fas fa-thermometer-quarter',
+  table:     'fas fa-table',
+  edit:      'fas fa-edit',
+  download:  'fas fa-download',
+  graph:     'fas fa-chart-line',
+});
+
+module.exports = { icons } ;
